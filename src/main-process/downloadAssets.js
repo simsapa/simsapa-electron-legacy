5694936ce40c0270f9c1e0e275623c888aff3103
Gambhiro <gambhiro.bhikkhu.85@gmail.com>
2018-10-14 20:57:46 +0100

    download assets on first start

--- src/main-process/downloadAssets.js
+++ src/main-process/downloadAssets.js
@@ -7,115 +7,145 @@ const progress = require('request-progress');
 const bizzy = require('bizzy');
 const md5File = require('md5-file');
 
-const {BrowserWindow, ipcMain, shell} = require('electron');
+const { app, ipcMain } = require('electron');
 
 const version = require('../version');
 
-/*
-const dbTarName = "appdata.tar.bz2";
-const staticTarName = "static-assets.tar.bz2";
+let downloading = false;
 
-const dbAsset = {
-    tarName: dbTarName,
-    msgId: 'database',
-    tarUrl: `http://ssp.a-buddha-ujja.org/v0.1.0/${dbTarName}`,
-    tarSavePath: path.join(__dirname, "..", dbTarName),
-    extractDir: path.join(__dirname, ".."),
-};
+ipcMain.on('download', (event) => {
+    if (downloading === true) {
+        return;
+    }
 
-const staticAsset = {
-    tarName: staticTarName,
-    msgId: 'assets',
-    tarUrl: `http://ssp.a-buddha-ujja.org/v0.1.0/${staticTarName}`,
-    tarSavePath: path.join(__dirname, "..", staticTarName),
-    extractDir: path.join(__dirname, ".."),
-};
-*/
+    downloading = true;
 
-ipcMain.on('download-database', (event) => {
-    downloadAssetTarball(event, "database", version.localVersion.appdata_tar);
-});
+    var all_opts = [
+        {
+            event: event,
+            msgId: 'assets',
+            assetVersion: version.localVersion.assets_tar,
+        },
+        {
+            event: event,
+            msgId: 'database',
+            assetVersion: version.localVersion.appdata_tar,
+        },
+    ];
 
-ipcMain.on('download-assets', (event) => {
-    downloadAssetTarball(event, "assets", version.localVersion.assets_tar);
-});
-
-function downloadAssetTarball(event, msgId, assetVersion) {
-    let tarSavePath = path.join(__dirname, "..", assetVersion.saveAs);
-
-    progress(request(assetVersion.url))
-        .on('progress', (state) => {
-            const msg = "Downloading: " + (state.percent.toFixed(2) * 100) + "%";
-            event.sender.send(`${msgId}-info`, msg);
-        })
-        .on('error', (err) => {
-            event.sender.send(`${msgId}-error`, err);
-        })
-        .on('end', () => {
-            const msg = `Downloaded to: ${tarSavePath}`;
-            event.sender.send(`${msgId}-info`, msg);
-            md5CheckTar(event, msgId, assetVersion);
-        })
-        .pipe(fs.createWriteStream(tarSavePath));
-}
-
-function md5CheckTar(event, msgId, assetVersion) {
-    let tarSavePath = path.join(__dirname, "..", assetVersion.saveAs);
-
-    md5File(tarSavePath, (err, hash) => {
-        if (err) {
-            const msg = `Download integrity check error: ${err}`;
-            event.sender.send(`${msgId}-error`, msg);
-            return;
-        }
-
-        if (assetVersion.md5.trim() == hash.trim()) {
-            const msg = "Download integrity verified.";
-            event.sender.send(`${msgId}-info`, msg);
-
-            extractAsset(event, msgId, assetVersion);
-
-        } else {
-            const msg = "Download integrity check failed.";
-            event.sender.send(`${msgId}-error`, msg);
-        }
-    });
-}
-
-function extractAsset(event, msgId, assetVersion) {
-    let tarSavePath = path.join(__dirname, "..", assetVersion.saveAs);
-    let extractDir = path.join(__dirname, "..");
-
-    const extract = bizzy(tarSavePath, extractDir);
-
-    event.sender.send(`${msgId}-info`, "Extracting...");
-
-    /*
-    extract.on('file', (name) => {
-        const msg = `Extracted: ${name}%`;
-        event.sender.send(`${msgId}-info`, msg);
-    });
-    */
-
-    // This doesn't actually show progress. It shows 100% at one point but the
-    // extraction is not yet finished.
-
-    /*
-    extract.on('progress', (percent) => {
-        const msg = `Extracting: ${percent}%`;
-        event.sender.send(`${msgId}-info`, msg);
-    });
-    */
-
-    extract.on('error', (err) => {
-        event.sender.send(`${msgId}-error`, err);
-    });
-
-    extract.on('end', () => {
-        event.sender.send(`${msgId}-info`,  "Successfully extracted.");
-
-        fs.unlink(tarSavePath, (err) => {
-            if (err) throw err;
+    var processAsset = function(opts) {
+        return new Promise((resolve, reject) => {
+            downloadAssetTarball(opts)
+                .then(() => { return md5CheckTar(opts); })
+                .then(() => { resolve(extractAsset(opts)); })
+                .catch((err) => {
+                    opts.event.sender.send(`${opts.msgId}-error`, err);
+                    reject(false);
+                });
         });
+    };
+
+    var actions = all_opts.map(processAsset);
+
+    var results = Promise.all(actions);
+
+    results
+        .then(() => {
+            downloading = false;
+            event.sender.send("show-restart");
+        })
+        .catch((err) => {
+            console.log(err);
+            throw err;
+        });
+
+});
+
+ipcMain.on('restart', (event) => {
+    app.relaunch();
+    app.quit();
+});
+
+function downloadAssetTarball(opts) {
+    return new Promise((resolve, reject) => {
+        let tarSavePath = path.join(__dirname, "..", opts.assetVersion.saveAs);
+
+        progress(request(opts.assetVersion.url))
+            .on('progress', (state) => {
+                let p = Math.floor(parseFloat(state.percent)*100);
+                opts.event.sender.send(`${opts.msgId}-info`,  `Downloading ${opts.assetVersion.saveAs}: ${p}%`);
+                opts.event.sender.send(`${opts.msgId}-progress`,  p);
+            })
+            .on('error', (err) => {
+                reject(err);
+            })
+            .on('end', () => {
+                opts.event.sender.send(`${opts.msgId}-info`, "");
+                opts.event.sender.send(`${opts.msgId}-progress`, null);
+                resolve(true);
+            })
+            .pipe(fs.createWriteStream(tarSavePath));
+    });
+}
+
+function md5CheckTar(opts) {
+    return new Promise((resolve, reject) => {
+        let tarSavePath = path.join(__dirname, "..", opts.assetVersion.saveAs);
+
+        md5File(tarSavePath, (err, hash) => {
+            if (err) {
+                opts.event.sender.send(`${opts.msgId}-error`, err);
+                reject(err);
+            }
+
+            if (opts.assetVersion.md5.trim() == hash.trim()) {
+                resolve(true);
+            } else {
+                const msg = "Download integrity check failed.";
+                opts.event.sender.send(`${opts.msgId}-error`, msg);
+                resolve(msg);
+            }
+        });
+    });
+}
+
+function extractAsset(opts) {
+    return new Promise((resolve, reject) => {
+        let tarSavePath = path.join(__dirname, "..", opts.assetVersion.saveAs);
+        let extractDir = path.join(__dirname, "..");
+
+        opts.event.sender.send(`${opts.msgId}-info`, `Extracting ${opts.assetVersion.saveAs} ...`);
+
+        const extract = bizzy(tarSavePath, extractDir);
+
+        extract.on('error', (err) => {
+            opts.event.sender.send(`${opts.msgId}-error`, err);
+            reject(err);
+        });
+
+        extract.on('end', () => {
+            opts.event.sender.send(`${opts.msgId}-info`,  "Completed.");
+
+            fs.unlink(tarSavePath, (err) => {
+                if (err) throw err;
+            });
+
+            resolve(true);
+        });
+
+        /*
+          extract.on('file', (name) => {
+          event.sender.send(`${msgId}-info`,  `Extracted: ${name}%`);
+          });
+        */
+
+        // This doesn't actually show progress. It shows 100% at one point but the
+        // extraction is not yet finished.
+
+        /*
+          extract.on('progress', (percent) => {
+          event.sender.send(`${msgId}-info`,  `Extracting: ${percent}%`);
+          });
+        */
     });
 }
--- src/renderer-process/downloadAssets.js
+++ src/renderer-process/downloadAssets.js
@@ -1,32 +1,109 @@
 // downloadAssets.js: Render Process
 
-const {ipcRenderer} = require('electron');
+const { ipcRenderer } = require('electron');
 
-const downloadDatabaseBtn = document.getElementById('download-database');
+const downloadBtn = document.getElementById('download');
 
-downloadDatabaseBtn.addEventListener('click', (event) => {
-    ipcRenderer.send('download-database');
+downloadBtn.addEventListener('click', (event) => {
+    ipcRenderer.send('download');
 });
 
-ipcRenderer.on('database-error', (event, msg) => {
-    document.getElementById('database-error').innerHTML = msg;
+let state = {
+    database: {
+        info_text: null,
+        progress_percent: null,
+        error_text: null,
+    },
+    assets: {
+        info_text: null,
+        progress_percent: null,
+        error_text: null,
+    },
+};
+
+function infoTemplate(s) {
+    let res = "";
+
+    if (s.info_text !== null) {
+        res += `
+<div class="level">
+    <div class="level-item">${s.info_text}</div>
+</div>`;
+    }
+
+    if (s.progress_percent !== null) {
+        res += `
+<div class="level">
+    <div class="level-item">
+        <progress class="progress" value="${s.progress_percent}" max="100">${s.progress_percent}%</progress>
+    </div>
+</div>`;
+    }
+
+    if (s.error_text !== null) {
+        res += `
+<div class="level">
+    <div id="database-error" class="level-item">${s.error_text}</div>
+</div>`;
+    }
+
+    return res;
+}
+
+function updateContent(id, data) {
+    document.getElementById(id).innerHTML = infoTemplate(data);
+}
+
+ipcRenderer.on('database-progress', (event, percent) => {
+    state.database.progress_percent = percent;
+    updateContent('database-wrap', state.database);
 });
 
 ipcRenderer.on('database-info', (event, msg) => {
-    document.getElementById('database-info').innerHTML = msg;
+    state.database.info_text = msg;
+    updateContent('database-wrap', state.database);
 });
 
-const downloadAssetsBtn = document.getElementById('download-assets');
-
-downloadAssetsBtn.addEventListener('click', (event) => {
-    ipcRenderer.send('download-assets');
+ipcRenderer.on('database-error', (event, msg) => {
+    state.database.error_text = msg;
+    updateContent('database-wrap', state.database);
 });
 
-ipcRenderer.on('assets-error', (event, msg) => {
-    document.getElementById('assets-error').innerHTML = msg;
+ipcRenderer.on('assets-progress', (event, percent) => {
+    state.assets.progress_percent = percent;
+    updateContent('assets-wrap', state.assets);
 });
 
 ipcRenderer.on('assets-info', (event, msg) => {
-    document.getElementById('assets-info').innerHTML = msg;
+    state.assets.info_text = msg;
+    updateContent('assets-wrap', state.assets);
 });
+
+ipcRenderer.on('assets-error', (event, msg) => {
+    state.assets.error_text = msg;
+    updateContent('assets-wrap', state.assets);
+});
+
+ipcRenderer.on('show-restart', () => {
+    let content = `
+<div class="level">
+    <div class="level-item">
+        All tasks are completed. Click the button to restart the application.
+    </div>
+</div>
+<div class="level">
+    <div class="level-item button-wrap">
+        <button id="restart" class="button is-info">Restart</button>
+    </div>
+</div>`;
+
+    document.getElementById('restart-wrap').innerHTML = content;
+
+    document
+        .getElementById('restart')
+        .addEventListener('click', (event) => {
+            ipcRenderer.send('restart');
+        });
+});
+
 
