4257dcb89e49855de94d9b1f728ef886890074a7
Gambhiro <gambhiro.bhikkhu.85@gmail.com>
2021-02-13 17:50:51 +0000

    legacy config path

--- src/app_info.js
+++ src/app_info.js
@@ -1,7 +1,7 @@
 import { app } from 'electron';
 const path = require('path');
 
-export const simsapaAppDataPath = path.join(app.getPath('appData'), 'simsapa');
+export const simsapaAppDataPath = path.join(app.getPath('appData'), 'simsapa-legacy');
 export const simsapaSrcPath = __dirname;
 export const dbPath = path.join(simsapaAppDataPath, "appdata.sqlite3");
 export const assetsPath = path.join(simsapaAppDataPath, "static");
